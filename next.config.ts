312bac89f68c9fadd85da58a1b1caedec3cbed07
M Anas Latif <m.anaslatif.com@gmail.com>
2025-09-24 07:34:54 +0500

    feat: configure Next.js settings with strict mode, minification, TypeScript, ESLint, and image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,48 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mosaic.scdn.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lineup-images.scdn.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wrapped-images.spotifycdn.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'seeded-session-images.scdn.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'newjams-images.scdn.co',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
